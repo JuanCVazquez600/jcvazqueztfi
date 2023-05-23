7bd56fe0cb8dfa064f059f631d389c28eb01523d
unknown <Juan Cruz>
2023-05-23 19:44:56 -0300

    Primer commit

---
+++ src/main/java/com/unla/jcvazqueztfi/JcvazqueztfiApplication.java
@@ -0,0 +1,13 @@
+package com.unla.jcvazqueztfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcvazqueztfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcvazqueztfiApplication.class, args);
+	}
+
+}
